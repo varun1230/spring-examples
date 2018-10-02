42499548f8e3e0b44fdced7aa920031fea74deac
swathi varun <swathi varun@LAPTOP-3JGLOADQ>
2018-10-01 22:59:46 -0500

    spring aop changes with request mapping

--- spring-aop/src/main/java/org/vstech/spring/controller/MessagingRestController.java
+++ spring-aop/src/main/java/org/vstech/spring/controller/MessagingRestController.java
@@ -8,18 +8,21 @@ package org.vstech.spring.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagingRestController {
 
-	@RequestMapping(value="/printMessage/${id}")
-	public void printMessage(@RequestParam String id, HttpServletRequest request, HttpServletResponse response){	
-	
-	System.out.println("Request Recevied is"+id);
+	@RequestMapping(value="/printMessage" , method=RequestMethod.GET)
+//	public String printMessage(@PathVariable("id") String id, HttpServletRequest request, HttpServletResponse response){	
+	public String printMessage(@RequestParam String id){	
+		
+		return "Some thing to print as the response";
 	}
 	
 }
