3b4ce00f4e54f2fb33acf5c1dc039a4d4e34f425
Varun Dosapati <vspsvk@gmail.com>
2018-10-15 10:23:23 -0500

    commiting modifications

--- spring-boot/src/main/java/org/vstech/spring/boot/SpringBootRestMainApp.java
+++ spring-boot/src/main/java/org/vstech/spring/boot/SpringBootRestMainApp.java
@@ -7,15 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * http://websystique.com/spring-boot/spring-boot-rest-api-example/
  * https://stackoverflow.com/questions/28466207/could-not-find-acceptable-representation-using-spring-boot-starter-web
  * https://stackoverflow.com/questions/26549379/when-use-responseentityt-and-restcontroller-for-spring-restful-applications
- * 
  *
  */
 
 //same as @Configuration @EnableAutoConfiguration @ComponentScan
-@SpringBootApplication(scanBasePackages={"org.vstech.spring.boot.rest.controller"})
+
+@SpringBootApplication(scanBasePackages={"org.vstech.spring.boot"})
 public class SpringBootRestMainApp {
  
 	public static void main(String[] args) {
-		SpringApplication .run(SpringBootRestMainApp.class, args);
+		SpringApplication.run(SpringBootRestMainApp.class, args);
 	}
 }
