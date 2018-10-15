d6c7d3083a5fd4896ca98c7420cef2df32743c28
Varun Dosapati <vspsvk@gmail.com>
2018-10-15 10:31:16 -0500

    fixing issue

--- spring-boot/src/main/java/org/vstech/spring/boot/SpringBootRestMainApp.java
+++ spring-boot/src/main/java/org/vstech/spring/boot/SpringBootRestMainApp.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.vstech.spring.boot;
 
 import org.springframework.boot.SpringApplication;
@@ -20,26 +19,3 @@ public class SpringBootRestMainApp {
 		SpringApplication.run(SpringBootRestMainApp.class, args);
 	}
 }
-=======
-package org.vstech.spring.boot;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-/**
- * http://websystique.com/spring-boot/spring-boot-rest-api-example/
- * https://stackoverflow.com/questions/28466207/could-not-find-acceptable-representation-using-spring-boot-starter-web
- * https://stackoverflow.com/questions/26549379/when-use-responseentityt-and-restcontroller-for-spring-restful-applications
- *
- */
-
-//same as @Configuration @EnableAutoConfiguration @ComponentScan
-
-@SpringBootApplication(scanBasePackages={"org.vstech.spring.boot"})
-public class SpringBootRestMainApp {
- 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestMainApp.class, args);
-	}
-}
->>>>>>> branch 'master' of https://github.com/varun1230/spring-examples.git
